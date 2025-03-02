8a2c3cc51dd92d8d6989cf5da18e6e14fd384957
shreyapandey <shreyapandeeyy@gmail.com>
2025-03-02 13:16:31 +0530

    Adding background

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Modale } from "../components/modale";
+import { Sky } from "../components/Sky";
 
 function index() {
   return (
-    <div style={{ height: '80vh', width: '80vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Canvas>
         <Environment preset="sunset" />
         <OrbitControls />
+        <Sky />
         <Modale />
       </Canvas>
     </div>
